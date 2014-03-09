7e9a63ea3856504fc3ebc55830c98cf46ee6eb8e
bigwheel <k.bigwheel+eng@gmail.com>
2014-03-10 00:46:20 +0900

    recipeの中身を書いた

--- recipes/default.rb
+++ recipes/default.rb
@@ -6,3 +6,20 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+
+package 'git'
+
+bash 'install git-trash-branch' do
+  code <<-EOS
+   curl -o /usr/local/bin/git-trash-branch https://raw2.github.com/bigwheel/git-trash-branch/master/git-trash-branch
+   sudo chmod +x /usr/local/bin/git-trash-branch
+  EOS
+end
+
+if FileTest::directory?('/usr/local/share/zsh/site-functions/')
+  bash 'install zsh completion for git-trash-branch' do
+    code <<-EOS
+    curl -o /usr/local/share/zsh/site-functions/_git-trash-branch https://raw2.github.com/bigwheel/git-trash-branch/master/zsh/_git-trash-branch
+    EOS
+  end
+end
